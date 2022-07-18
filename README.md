e28d9f0e9ddb2da3e8c01a76b05eacd02cddfa4b
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-07-18 18:20:04 -0400

    initial commit of the `nylas-js` files

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# StandardTemplate
-Standard template with branch protections
+# Nylas JavaScript SDK
+
+![npm](https://img.shields.io/npm/v/@nylas/nylas-js)
+
+This is the GitHub repository for the Nylas JavaScript SDK. The Nylas Communications Platform provides REST APIs for [Email](https://developer.nylas.com/docs/connectivity/email/), [Calendar](https://developer.nylas.com/docs/connectivity/calendar/), and [Contacts](https://developer.nylas.com/docs/connectivity/contacts/), and the Node SDK is the quickest way to build your integration using JavaScript.
+
+Here are some resources to help you get started:
+
+- [Quickstart](https://developer.nylas.com/docs/the-basics/quickstart/)
+- [Nylas API Reference](https://developer.nylas.com/docs/api/)
+
+
+# Install
+
+To run the Nylas Node SDK, you will first need to have [npm](https://www.npmjs.com/get-npm) installed on your machine.
+
+Then, head to the nearest command line and run the following:
+```bash
+npm install @nylas/nylas-js
+```
+
+To install this package from source, clone this repo and run `npm install` from inside the project directory:
+
+```bash
+git clone https://github.com/nylas/nylas-js.git
+cd nylas-js
+npm install
+```
+
+# Usage
+
+The entrypoint for the SDK is `Nylas`, which can be imported as an ES module and instantiated:
+
+```javascript
+import Nylas from 'nylas-js'
+
+const nylas = new Nylas({
+  // Config
+});
+```
+
+### Configuration options
+These are the following options that can be passed in to configure an instance of the Nylas JS SDK
+
+#### `serverBaseUrl`
+The URL of the backend server configured with the Nylas middleware or Nylas routes implemented.
+
+# Contributing
+
+Please refer to [Contributing](Contributing.md) for information about how to make contributions to this project. We welcome questions, bug reports, and pull requests.
+
+# License
+
+This project is licensed under the terms of the MIT license. Please refer to [LICENSE](LICENSE.txt) for the full terms. 
+
+
---
+++ src/index.ts
@@ -0,0 +1,100 @@
+enum DefaultEndpoints {
+  GenerateAuthUrl = '/nylas/generate-auth-url',
+  ExchangeMailboxToken = '/nylas/exchange-mailbox-token'
+}
+
+export interface NylasProps {
+  serverBaseUrl: string
+}
+
+export interface AuthUrlOptions {
+  successRedirectUrl: string,
+  emailAddress?: string,
+  generateAuthUrlEndpoint?: string,
+  onConnectionError?: (error: Error) => void
+}
+
+export interface ExchangeCodeOptions {
+  exchangeCodeForTokenEndpoint?: string,
+  onConnectionError?: (error: Error) => void
+}
+
+export default class Nylas {
+  serverBaseUrl: string
+
+  constructor(props: NylasProps) {
+    this.serverBaseUrl = props.serverBaseUrl;
+  }
+
+  async buildAuthUrl(opts: AuthUrlOptions): Promise<string | boolean> {
+    try {
+      const url =
+        this.serverBaseUrl +
+        (opts.generateAuthUrlEndpoint ||
+          DefaultEndpoints.GenerateAuthUrl)
+      const rawResp = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email_address: opts.emailAddress,
+          success_url: opts.successRedirectUrl,
+        }),
+      })
+
+      return await rawResp.text();
+    } catch (e: any) {
+      console.warn(`Error fetching auth URL:`, e)
+      opts.onConnectionError && opts.onConnectionError(e);
+      return false;
+    }
+  }
+
+  async authWithRedirect(opts: AuthUrlOptions): Promise<void | boolean> {
+    const authUrl = await this.buildAuthUrl(opts);
+    if(authUrl !== false && typeof authUrl === 'string') {
+      window.location.href = authUrl;
+    }
+
+    return false;
+  }
+
+  async exchangeCodeForToken(authorizationCode: string, opts?: ExchangeCodeOptions): Promise<string | boolean> {
+    try {
+      if (!authorizationCode) {
+        return false
+      }
+
+      const url =
+        this.serverBaseUrl +
+        (opts?.exchangeCodeForTokenEndpoint ||
+          DefaultEndpoints.ExchangeMailboxToken)
+      const rawResp = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          token: authorizationCode,
+        }),
+      })
+      return await rawResp.text()
+    } catch (e: any) {
+      console.warn(`Error exchanging mailbox token:`, e)
+      opts?.onConnectionError && opts.onConnectionError(e)
+      return false
+    }
+  }
+
+  async exchangeCodeFromUrlForToken(opts?: ExchangeCodeOptions): Promise<string | boolean> {
+    const authorizationCode = new URLSearchParams(window.location.search).get(
+      'code'
+    );
+    if (!authorizationCode) {
+      return false
+    }
+
+    return await this.exchangeCodeForToken(authorizationCode, opts);
+  }
+}
